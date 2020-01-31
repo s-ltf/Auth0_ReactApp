ba4fb9cbf1009a66ace801491d9929e0df3de562
s-ltf <iras146@gmail.com>
2020-01-31 11:16:28 -0700

    Updates

---
+++ precache-manifest.678e9d3b71cf3ca72284a2141e9d6516.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20a2b70c0e52a4184387f84c2ada52f",
+    "url": "/Auth0_ReactApp/index.html"
+  },
+  {
+    "revision": "9a69f350ccfb95d2084d",
+    "url": "/Auth0_ReactApp/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "1c00a0988c3427a05133",
+    "url": "/Auth0_ReactApp/static/css/main.4d89ea0b.chunk.css"
+  },
+  {
+    "revision": "9a69f350ccfb95d2084d",
+    "url": "/Auth0_ReactApp/static/js/2.e8ee9867.chunk.js"
+  },
+  {
+    "revision": "1c00a0988c3427a05133",
+    "url": "/Auth0_ReactApp/static/js/main.e7d4a8d8.chunk.js"
+  },
+  {
+    "revision": "a12dfbe8987202ab8c88",
+    "url": "/Auth0_ReactApp/static/js/runtime~main.9a9fa874.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/Auth0_ReactApp/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Auth0_ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
